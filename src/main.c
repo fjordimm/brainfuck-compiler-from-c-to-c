0bed0eaa9d4b6c91cc015cc97c330fa0a240f2a0
fjordimm <fjordimm@gmail.com>
2023-03-12 21:04:47 -0700

    finished the compiler (as far as i can tell)

--- src/main.c
+++ src/main.c
@@ -29,8 +29,6 @@ const char OUTFILENAME[] = "..\\out\\out.c";
 
 ///// Forward Declarations /////
 
-char* getContentsOfFile(const char*);
-void writeToFile(const char*, char*);
 bool isBrainfuckSymbol(char);
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -39,76 +37,98 @@ bool isBrainfuckSymbol(char);
 
 int main(void)
 {
-	char* brainfuckCode = getContentsOfFile(INFILENAME);
+	// Reading In file
+	char* brainfuckCode;
+	{
+		FILE* file = fopen(INFILENAME, "r");
 
-	char testThing[] = "bruh moment cringe";
+		if (file == NULL)
+		{
+			printf("Could not open file\n");
+			exit(0);
+		}
+		else
+		{
+			int fileSize;
+			fseek(file, 0L, SEEK_END);
+			fileSize = ftell(file);
+			fseek(file, 0L, SEEK_SET);
+			
+			brainfuckCode = malloc(sizeof(char) * fileSize);
 
-	writeToFile(OUTFILENAME, testThing);
+			int cntr = 0;
+			char nextChar;
+			do
+			{
+				nextChar = fgetc(file);
+				if (isBrainfuckSymbol(nextChar))
+				{
+					brainfuckCode[cntr] = nextChar;
+					cntr++;
+				}
+			}
+			while (nextChar != EOF);
+			brainfuckCode[cntr] = '\0';
 
-	// int i = 0;
-	// while (brainfuckCode[i] != '\0')
-	// {
-	// 	printf("%c", brainfuckCode[i]);
-	// 	i++;
-	// }
+			fclose(file);
+		}
+	}
+
+	// Writing to Out file
+	{
+		FILE* file = fopen(OUTFILENAME, "w");
+
+		if (file == NULL)
+		{
+			printf("Could not open file\n");
+			exit(0);
+		}
+		else
+		{
+			// This is the main compiler part
+
+			fprintf(file, "#include <stdio.h>\n");
+			fprintf(file, "int main(void)\n{\n");
+			fprintf(file, "char arr[30000] = {0};\n");
+			fprintf(file, "char* ptr = arr;\n\n");
+
+			int i = 0;
+			while (brainfuckCode[i] != '\0')
+			{
+				char nex = brainfuckCode[i];
+
+				if (nex == '>')
+				{ fprintf(file, "ptr++;\n"); }
+				else if (nex == '<')
+				{ fprintf(file, "ptr--;\n"); }
+				else if (nex == '+')
+				{ fprintf(file, "*ptr++;\n"); }
+				else if (nex == '-')
+				{ fprintf(file, "*ptr--;\n"); }
+				else if (nex == '.')
+				{ fprintf(file, "putchar(*ptr);\n"); }
+				else if (nex == ',')
+				{ fprintf(file, "*ptr = getchar();\n"); }
+				else if (nex == '[')
+				{ fprintf(file, "while (*ptr)\n{\n"); }
+				else if (nex == ']')
+				{ fprintf(file, "}\n"); }
+
+				i++;
+			}
+
+			fprintf(file, "\nreturn 0;\n");
+			fprintf(file, "}\n");
+
+			fclose(file);
+		}
+	}
+
+	printf("Compilation successful\n");
 
 	free(brainfuckCode);
 
 	return 0;
-}
-
-char* getContentsOfFile(const char* fileName)
-{
-	FILE* file = fopen(fileName, "r");
-
-	if (file == NULL)
-	{
-		printf("Could not open file\n");
-		return NULL;
-	}
-	else
-	{
-		int fileSize;
-		fseek(file, 0L, SEEK_END);
-		fileSize = ftell(file);
-		fseek(file, 0L, SEEK_SET);
-		
-		char* ret = malloc(sizeof(char) * fileSize);
-
-		int cntr = 0;
-		char nextChar;
-		do
-		{
-			nextChar = fgetc(file);
-			if (isBrainfuckSymbol(nextChar))
-			{
-				ret[cntr] = nextChar;
-				cntr++;
-			}
-		}
-		while (nextChar != EOF);
-		ret[cntr] = '\0';
-
-		fclose(file);
-		return ret;
-	}
-}
-
-void writeToFile(const char* fileName, char* text)
-{
-	FILE* file = fopen(fileName, "w");
-
-	if (file == NULL)
-	{
-		printf("Could not open file\n");
-	}
-	else
-	{
-		fprintf(file, "hohoho bitches");
-		fprintf(file, "bingus schmingus");
-
-		fclose(file);
-	}
 }
 
 bool isBrainfuckSymbol(char the)
